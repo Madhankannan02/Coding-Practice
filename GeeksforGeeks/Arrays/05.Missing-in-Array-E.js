93f3c9e0becfb3e1a9f73d8990d50820547f0c16
Madhankannan02 <madhankannan29@gmail.com>
2026-03-03 20:37:11 +0530

    05.Missing-in-Array-E'

---
+++ GeeksforGeeks/Arrays/05.Missing-in-Array-E.js
@@ -0,0 +1,13 @@
+class Solution {
+    missingNum(arr) {
+        let n = arr.length + 1;
+
+        let expectedSum = (n * (n + 1)) / 2;
+        
+        let actualSum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            actualSum += arr[i];
+        }
+        return expectedSum - actualSum;
+    }
+}
