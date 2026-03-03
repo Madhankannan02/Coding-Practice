af2d79fb6e56ac2268993396c0e178ff51d38388
Madhankannan02 <madhankannan29@gmail.com>
2026-03-03 20:38:45 +0530

    06.Array-Duplicates-E

---
+++ GeeksforGeeks/Arrays/06.Array-Duplicates-E.js
@@ -0,0 +1,18 @@
+class Solution {
+    findDuplicates(arr) {
+        let seen = new Set();
+        let duplicates = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            let num = arr[i];
+            
+            if (seen.has(num)) {
+                duplicates.push(num);
+            } else {
+                seen.add(num);
+            }
+        }
+
+        return duplicates;
+    }
+}
