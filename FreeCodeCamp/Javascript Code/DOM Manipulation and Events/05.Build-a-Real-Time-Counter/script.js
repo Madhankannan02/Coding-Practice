0d69f5a0c12386830e2201c766a0c3ca6668995a
Madhankannan02 <madhankannan29@gmail.com>
2026-04-03 22:06:27 +0530

    Corrected the error in the script.js to change the color

--- FreeCodeCamp/Javascript Code/DOM Manipulation and Events/05.Build-a-Real-Time-Counter/script.js
+++ FreeCodeCamp/Javascript Code/DOM Manipulation and Events/05.Build-a-Real-Time-Counter/script.js
@@ -1,16 +1,16 @@
 const textInput = document.getElementById("text-input");
 const charCount = document.getElementById("char-count");
-const MAX_CHARS = 50;
 
 textInput.addEventListener("input", () => {
   let content = textInput.value;
 
-  if (content.length >= MAX_CHARS) {
-    textInput.value = content.substring(0, MAX_CHARS);
+  if (content.length >= 50) {
+    textInput.value = content.substring(0, 50);
     content = textInput.value;
-    charCount.classList.add("limit-reached");
+    charCount.style.color = "red";
   } else {
-    charCount.classList.remove("limit-reached");
+    charCount.style.color = "black";
   }
-  charCount.textContent = `Character Count: ${content.length}/${MAX_CHARS}`;
+
+  charCount.textContent = `Character Count: ${content.length}/50`;
 });
